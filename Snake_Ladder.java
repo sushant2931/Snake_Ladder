428472d96259eaa5ce3071d742d7b759b1bdf3c8
sushant2931 <sushantlad8888@gmail.com>
2021-08-29 23:42:12 +0530

    Roll the Die

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -4,7 +4,21 @@ public class Snake_Ladder {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		// initialise position as 0
 		int position = 0;
+		int min = 1;
+		int max = 6;
+		System.out.println("Press 1 for Roll the Die :");
+		int number = sc.nextInt();
+		if (number == 1) {
+
+			int dieNumber = (int) (Math.random() * (max - min + 1) + min);
+			System.out.println("Dies number : " + dieNumber);
+		} else {
+
+			System.out.println("Not Valid");
+			sc.close();
+		}
 	}
 }
